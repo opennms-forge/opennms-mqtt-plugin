4f6b9595efafbdaa09ec78e35916db4c947cd3bb
gallenc <cgallen@opennms.org>
2023-08-03 18:35:37 +0100

    adding simulator for values

---
+++ integration-tests/collection-simulator/src/main/java/org/opennms/plugins/iot/collectionsim/CollectionSimulatorApplication.java
@@ -0,0 +1,13 @@
+package org.opennms.plugins.iot.collectionsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionSimulatorApplication.class, args);
+	}
+
+}
---
+++ integration-tests/collection-simulator/src/test/java/org/opennms/plugins/iot/collectionsim/CollectionSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opennms.plugins.iot.collectionsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
